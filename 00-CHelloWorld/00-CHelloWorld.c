b79e2dad729c27468c4eda6c6bdfed144d202341
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2023-04-07 19:51:40 -0300

    Primer commit, creacion del espacio de trabajo de SSL

---
+++ 00-CHelloWorld/00-CHelloWorld.c
@@ -0,0 +1,14 @@
+/**
+ * @file 00-CHelloWorld.c
+ * @author Eduardo Onishi (eonishi@frba.utn.edu.ar)
+ * @brief Hello World in C
+ * @date 2023-03-29
+ */
+
+#include<stdio.h> 
+
+ int main (void) 
+ { 
+    printf ("Hello World! \n ");
+    return 0 ;
+}
